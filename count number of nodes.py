a832386fadf60b31ea1cdbab928580e895e4bf50
sunnysunita <sunnysunita.com@gmail.com>
2020-05-03 18:39:46 +0530

    final

---
+++ count number of nodes.py
@@ -0,0 +1,48 @@
+class GenericTree:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+
+def generic_tree():
+    root_data = int(input("Enter the root data: "))
+
+    # edge case only
+    if root_data == -1:
+        return None
+
+    root = GenericTree(root_data)
+    num_children = int(input("Enter the number of children of "+str(root_data)+": "))
+    for i in range(num_children):
+        root.children.append(generic_tree())
+    return root
+
+def print_generic_tree(root):
+    # edge case only
+    if root is None:
+        return
+    print(root.data)
+    for e in root.children:
+        print_generic_tree(e)
+
+def print_generic_tree_detailed(root):
+    if root is None:
+        return None
+    print(root.data, end=":")
+    for e in root.children:
+        print(e.data, end=",")
+    print()
+    for e in root.children:
+        print_generic_tree_detailed(e)
+
+def num_nodes(root):
+    if root is None:
+        return 0
+    count = 1
+    for child in root.children:
+        count = count + num_nodes(child)
+    return count
+
+
+root = generic_tree()
+print_generic_tree_detailed(root)
+print(num_nodes(root))
