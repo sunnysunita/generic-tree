231005090ed70197c5fa7b2f8fd446ef2955027c
sunnysunita <sunnysunita.com@gmail.com>
2020-05-03 21:02:26 +0530

    final

---
+++ Count leaf Nodes.py
@@ -0,0 +1,40 @@
+class treeNode:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+    def __str__(self):
+        return str(self.data)
+
+def leafNodeCount(root):
+    #############################
+    # PLEASE ADD YOUR CODE HERE #
+    #############################
+    count = 0
+    if root is None:
+        return count
+    if len(root.children) == 0:
+        count += 1
+        return count
+    for child in root.children:
+        count = count + leafNodeCount(child)
+    return count
+def createLevelWiseTree(arr):
+    root = treeNode(int(arr[0]))
+    q = [root]
+    size = len(arr)
+    i = 1
+    while i<size:
+        parent = q.pop(0)
+        childCount = int(arr[i])
+        i += 1
+        for j in range(0,childCount):
+            temp = treeNode(int(arr[i+j]))
+            parent.children.append(temp)
+            q.append(temp)
+        i += childCount
+    return root
+
+# Main
+arr = list(int(x) for x in input().strip().split(' '))
+tree = createLevelWiseTree(arr)
+print(leafNodeCount(tree))
